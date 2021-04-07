e75d95e9b5ae9aaaf7bfb8c9202998d88157e814
qbacreative <qbacreative@gmail.com>
2021-04-07 13:54:17 -0400

    Fonts, Colors, Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
 
-const MaterialComponents = [
-  MatButtonModule
+
+const Material = [
+  MatButtonModule,
+  MatIconModule,
+  MatToolbarModule
 ];
 
 @NgModule({
   imports: [
-    MaterialComponents
+    Material
   ],
   exports: [
-    MaterialComponents
+    Material
   ]
 })
 export class MaterialModule { }
